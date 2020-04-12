2c2e612e969b21283c69e61f45a0323ff9962203
sakurai <39reyskhky@gmail.com>
2020-04-12 22:15:11 +0900

    codestyle修正

--- app/core/models.py
+++ app/core/models.py
@@ -2,25 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
-  def create_user(self, email, password=None, **extra_fields):
-    """Create and save a new user"""
-    if not email:
-      raise ValueError('Users must have email address')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a new user"""
+        if not email:
+            raise ValueError('Users must have email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-    return user
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """Custom user model that supports using email instead of username"""
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
