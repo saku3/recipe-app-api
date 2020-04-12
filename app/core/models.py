bb017b51cb65ff1a7131f14e791f883fdf3d50f6
sakurai <39reyskhky@gmail.com>
2020-04-12 22:33:58 +0900

    customusermodel追加

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -6,8 +6,8 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
         """Test creating a new user with an email is successful"""
-        email = "test@test.com"
-        password = "password"
+        email = 'test@test.com'
+        password = 'password'
         user = get_user_model().objects.create_user(
           email=email,
           password=password
@@ -18,8 +18,8 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = "test@TEST.COM"
-        user = get_user_model().objects.create_user(email, "password")
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'password')
 
         self.assertEqual(user.email, email.lower())
 
@@ -27,3 +27,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'password')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+          'test@test.com',
+          'password'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
