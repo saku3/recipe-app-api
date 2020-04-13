eaaca390486fd0f6101d08718108c4d8b98d57ed
sakurai <39reyskhky@gmail.com>
2020-04-14 01:20:08 +0900

    user更新追加

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -12,9 +12,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-    def create(self, validate_data):
+    def create(self, validated_data):
         """Create a new user with encripted password and return it"""
-        return get_user_model().objects.create_user(**validate_data)
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
