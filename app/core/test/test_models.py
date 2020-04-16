7c98f3324ccfeeecb6af2028b31679057f5e91d7
sakurai <39reyskhky@gmail.com>
2020-04-16 23:27:41 +0900

    Tag追加

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@test.com', password='password'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -9,8 +16,8 @@ class ModelTests(TestCase):
         email = 'test@test.com'
         password = 'password'
         user = get_user_model().objects.create_user(
-          email=email,
-          password=password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -31,9 +38,18 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-          'test@test.com',
-          'password'
+            'test@test.com',
+            'password'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='testtag'
+        )
+
+        self.assertEqual(str(tag), tag.name)
